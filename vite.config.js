200ecdd4d712b99d756cd605ecb350e04175aa77
Sam Hill <samuhill@gmail.com>
2025-11-14 10:31:08 -0500

    Fix Vite asset handling for uppercase PNG

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  assetsInclude: ['**/*.jpg', '**/*.png'], // Include your asset types here
+  assetsInclude: ["**/*.jpg", "**/*.png", "**/*.PNG"],
 });
